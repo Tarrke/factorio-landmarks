96a5143fae9fca67912d5684bcaedf478171a7a0
Tarrke <tarrke@gmail.com>
2018-07-03 12:03:00 +0200

    Rework the presentation

--- src/prototypes/item.lua
+++ src/prototypes/item.lua
@@ -1,10 +1,20 @@
-data:extend({
-    {
-        type = "item",
-        name = "eiffel_tower",
-        icon = "__landmarks__/graphics/icons/eiffel_tower.png",
-        flags = {"goes-to-quickbar"},
-        subgroup = "my_sub",
-        stack_size = 1,
-    },
-})
+-- item.lua
+
+local eiffelTower = {}
+
+eiffelTower.type = "item"
+eiffelTower.name = "eiffel-tower"
+eiffelTower.icon = "__landmarks__/graphics/icons/eiffel_tower.png"
+eiffelTower.flags = {"goes-to-quickbar"}
+eiffelTower.subgroup = "Landmarks"
+eiffelTower.stack_size = 1
+
+local eiffelTowerRevipe = {}
+
+eiffelTowerRevipe.type = "recipe"
+eiffelTowerRevipe.name = "eiffel-tower-recipe"
+eiffelTowerRevipe.enabled = "true"
+eiffelTowerRevipe.ingredients = { {"iron-stick", 100} }
+eiffelTowerRevipe.result = "effeil-tower"
+
+data:extend({eiffelTower, eiffelTowerRevipe})
